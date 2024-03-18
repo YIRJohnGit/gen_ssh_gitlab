cc7b0035416372b2cfdd7297fdb2ced62845e059
Irudaya Raj John Y <111234771+YIRJohnGit@users.noreply.github.com>
2024-03-18 11:34:18 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -12,3 +12,20 @@ color_echo()
 }
 
 color_echo "green" "Objective: SSH configuration for setting up for CICD"
+
+color_echo "yellow" "Enter an email OR your name OR ID [yi***@gmail.com]: "
+read user_ssh_input
+
+ssh-keygen -t rsa -b 2048 -C "${user_ssh_input}"
+cat ~/.ssh/id_rsa.pub | cat >> ~/.ssh/authorized_keys
+sudo chmod 700 ~/.ssh
+sudo chmod 600 ~/.ssh/authorized_keys
+
+echo "{
+Need to add he below be if not added earlier
+sudo vim /etc/ssh/sshd_config
+PubkeyAuthentication yes
+PasswordAuthentication no
+}"
+sudo systemctl restart sshd
+
